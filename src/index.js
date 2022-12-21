923d5a2518c0822c943b05ef3fe1b67b5813f290
Karlushyna Lesya <karlushyna.l@gmail.com>
2022-12-21 22:38:05 +0000

    upd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-import './css/styles.css';
+const getItemTemplate = ({ isDone, text }) => 
+  `  <li class="item">
+      <div>
+        <input type="checkbox" ${isDone ? 'checked' : ''} />
+        <span>${text}</span>
+      </div>
+      <button type="button">x</button>
+    </li>`; 
 
-const DEBOUNCE_DELAY = 300;
+    const refs = {
+        list: document.querySelector('.list'),
+    }
+
+const render = () => {
+const lis = items.map((item) => getItemTemplate(item));
+
+refs.list.innerHTML = '';
+refs.list.insertAdjacentHTML('beforeend', lis.join(''));
+
+}
+
+const items = [
+    {
+      text: 'to buy bread',
+      isDone: false,  
+    },
+    {
+        text: 'to buy milk',
+        isDone: true,  
+      },
+      {
+        text: 'to top up phone',
+        isDone: false,  
+      }
+]
+
+render();
+
+
+
